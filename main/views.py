d9a98dccee43649e7c6568450dff578721d78470
Trongtinne <149985316+Trongtinne@users.noreply.github.com>
2024-09-25 10:01:59 +0700

    main

---
+++ main/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.shortcuts import render
+from module_group.models import ModuleGroup, Module
+
+def home(request):
+    module_groups = ModuleGroup.objects.all()
+    modules = Module.objects.all()
+    return render(request, 'home.html', {
+        'module_groups': module_groups,
+        'modules': modules,
+    })
+
+
+def base_view(request):
+    module_groups = ModuleGroup.objects.all()
+    modules = Module.objects.all()
+    return render(request, 'base.html', {
+        'module_groups': module_groups,
+        'modules': modules,
+    })
+
+
+# from django.shortcuts import render
+# from django.contrib.auth.decorators import login_required
+
+# @login_required
+# def profile(request):
+#     return render(request, 'profile.html')

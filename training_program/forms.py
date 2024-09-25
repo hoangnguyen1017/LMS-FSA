54b85397505069a03fd63a55d336d6d355d03376
Trongtinne <149985316+Trongtinne@users.noreply.github.com>
2024-09-25 09:15:27 +0700

    Delete training_program directory

--- training_program/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import TrainingProgram
-
-# Form for creating and editing training programs
-class TrainingProgramForm(forms.ModelForm):
-    class Meta:
-        model = TrainingProgram
-        fields = ['program_name', 'program_code', 'description']
-

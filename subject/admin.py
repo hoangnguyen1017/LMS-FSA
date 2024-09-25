65bb07b4e1017144f4d057c65843fcb8770d7bb7
Trongtinne <149985316+Trongtinne@users.noreply.github.com>
2024-09-25 09:15:08 +0700

    Delete subject directory

--- subject/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Subject
-
-admin.site.register(Subject)
--- subject/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Subject
-
-# Form for creating and editing subjects
-class SubjectForm(forms.ModelForm):
-    class Meta:
-        model = Subject
-        fields = ['name', 'description']
-

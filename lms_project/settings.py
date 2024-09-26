1173291b2293f4debeb01812f62a1143a0742dfb
Escanole <147304808+Escanole@users.noreply.github.com>
2024-09-26 10:07:09 +0700

    Update settings.py

--- lms_project/settings.py
+++ lms_project/settings.py
@@ -103,10 +103,24 @@ WSGI_APPLICATION = 'lms_project.wsgi.application'
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Group2Internship$default',
+        'USER': 'Group2Internship',
+        'PASSWORD': '4321$#@!',
+        'HOST': 'Group2Internship.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+        'OPTIONS': {
+'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+}
     }
 }
 

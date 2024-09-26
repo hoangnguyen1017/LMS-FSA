ff5312bc8a746f5130f47771019e9558657c2882
Escanole <trankhanhnguyen03072004@gmail.com>
2024-09-26 04:35:28 +0000

    update

--- lms_project/settings.py
+++ lms_project/settings.py
@@ -158,7 +158,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Add this line if you have custom static directories
-STATIC_ROOT = '/home/Group2Internship/LMS-FSA/staticfiles'  # For production use
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For production use
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

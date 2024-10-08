6a990b8aa8e981ed1e266e44443c13ec86f5c6a3
Escanole <trankhanhnguyen03072004@gmail.com>
2024-10-08 09:41:02 +0000

    Update

--- subject/signals.py
+++ subject/signals.py
@@ -1,26 +1,25 @@
 import os
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
-from .models import Document, Video, Subject
+from .models import Document, Video, ReadingMaterial, SubjectMaterial
 
 @receiver(post_delete, sender=Document)
 def auto_delete_document_on_delete(sender, instance, **kwargs):
+    # Delete the associated file from the file system
     if instance.doc_file and os.path.isfile(instance.doc_file.path):
         os.remove(instance.doc_file.path)
+    # Delete the corresponding SubjectMaterial entry
+    SubjectMaterial.objects.filter(material_id=instance.id, material_type='document').delete()
 
 @receiver(post_delete, sender=Video)
 def auto_delete_video_on_delete(sender, instance, **kwargs):
+    # Delete the associated file from the file system
     if instance.vid_file and os.path.isfile(instance.vid_file.path):
         os.remove(instance.vid_file.path)
+    # Delete the corresponding SubjectMaterial entry
+    SubjectMaterial.objects.filter(material_id=instance.id, material_type='video').delete()
 
-@receiver(post_delete, sender=Subject)
-def auto_delete_subject_files(sender, instance, **kwargs):
-    # Delete associated documents
-    for document in Document.objects.filter(subject=instance):
-        if document.doc_file and os.path.isfile(document.doc_file.path):
-            os.remove(document.doc_file.path)
-
-    # Delete associated videos
-    for video in Video.objects.filter(subject=instance):
-        if video.vid_file and os.path.isfile(video.vid_file.path):
-            os.remove(video.vid_file.path)
+@receiver(post_delete, sender=ReadingMaterial)
+def auto_delete_reading_material_on_delete(sender, instance, **kwargs):
+    # No file to delete, but remove the SubjectMaterial entry
+    SubjectMaterial.objects.filter(material_id=instance.id, material_type='reading').delete()

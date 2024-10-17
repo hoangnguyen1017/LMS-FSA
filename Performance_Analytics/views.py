15b79b44116ca93e2c454c09fe27de995c9b5595
Trần Lê Duy <tranleduy@MacBookPro.lan>
2024-10-18 00:29:59 +0700

    Update

--- Performance_Analytics/views.py
+++ Performance_Analytics/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from .models import PerformanceAnalytics
 from user.models import User
-# Create your views here.
+from django.core.paginator import Paginator
 
-def performance_analytics_view(request):
-    analytics_data = PerformanceAnalytics.objects.all()
-    print(analytics_data)
-    return render(request, 'base.html', {'analytics_data': analytics_data})
-def home_view(request):
-    return render(request, 'home.html',{})
+def performance_analytics_summary(request):
+    user = request.user  
+    analytics = PerformanceAnalytics.objects.filter(user=user.id)
+    paginator = Paginator(analytics,3)
+    page_nummber = request.GET.get('page')
+    page_obj = paginator.get_page(page_nummber)
 
-def base_view(request):
-    return render(request, 'base.html',{})
-
+    context = {
+        'page_obj':page_obj,
+    }
+    return render(request, 'performance_analytics_summary.html', context)
--- user_progress/views.py
+++ user_progress/views.py
@@ -4,6 +4,11 @@ from course.models import Course, Enrollment
 from quiz.models import Quiz, StudentQuizAttempt
 from user.models import User
 from collections import Counter
+from django.core.paginator import Paginator
+from .models import UserCourseProgress
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
 
 # Create your views here.
 def calculate(user_id, course_id):
@@ -14,13 +19,13 @@ def calculate(user_id, course_id):
     return quizzes, attempts
 
 def progress_list(request):
-    # print(request.user.name)
+    print(request.user)
     module_groups = ModuleGroup.objects.all()
     modules = Module.objects.all()
     course = Enrollment.objects.filter(student=request.user)
     _dict = {"courses":None,
             "Percent":None}
-    list = []
+    _list = []
     for i in course:
         total, attempts = calculate(request.user, i.course.id)
         dict_ = dict(_dict)
@@ -29,10 +34,20 @@ def progress_list(request):
             dict_['Percent'] = 0
         else:
             dict_['courses'] = i
-            dict_['Percent'] = attempts / total * 100
-        list.append(dict_)
+            dict_['Percent'] = round(attempts / total * 100,2)
+        if len(list(UserCourseProgress.objects.filter(user=request.user, course_id=dict_['courses'].course.id))) !=0:
+            UserCourseProgress.objects.filter(user=request.user, course_id=dict_['courses'].course.id).update(
+                progress_percentage=dict_['Percent']
+            )
+        else:
+            UserCourseProgress.objects.create(
+            user=request.user, 
+            course=Course.objects.get(course_code=dict_['courses'].course.course_code), 
+            progress_percentage=dict_['Percent']
+        )
+        _list.append(dict_)
     return render(request,'aggregated.html',{'module_groups': module_groups,
                                              'modules': modules,
-                                             'courses': list,
-                                             'course_count':len(list),
+                                             'courses': _list,
+                                             'course_count':len(_list),
                                              'user': request.user })

09afd6eb7796a2eb4770a49ebc6864cdb046f48f
truong51972 <tranquoctruong20@gmail.com>
2024-11-07 20:19:48 +0700

    update script

--- scripts/update_django_backend_1.sh
+++ scripts/update_django_backend_1.sh
@@ -5,3 +5,7 @@ rm -rf "$TARGET_DIR"
 mkdir "$TARGET_DIR"
 
 git archive main | tar -x -C ./"$TARGET_DIR"
+
+git add "$TARGET_DIR"
+git commit -m "update django_backed from main branch to '$TARGET_DIR'"
+git push
